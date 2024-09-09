220702e089809c36a7fce9dc210e2fa27981826c
bbungjin <leejintwo12@naver.com>
2024-09-09 14:50:03 +0900

    close #4

--- day1/src/Test.jsx
+++ day1/src/Test.jsx
@@ -1,5 +1,12 @@
-const Test = () =>{
-    return 
+import { useState } from "react";
+
+const Test = () => {
+  const [state, setState] = useState();
+    return (
+        <div>
+            <input name="text" type="text"/>
+        </div>
+    );
 }
 
 export default Test;
