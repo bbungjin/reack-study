0202a8f9bfb78204214209b5b8cdc0485b2e82d4
bbungjin <leejintwo12@naver.com>
2024-09-09 15:21:28 +0900

    close #5

--- day1/src/Test.jsx
+++ day1/src/Test.jsx
@@ -1,10 +1,16 @@
 import { useState } from "react";
 
 const Test = () => {
-  const [state, setState] = useState();
+  const [state, setState] = useState({log:[]});
+  const logState = (e) => {
+    const {name, value} = e.target
+    return setState({value,log:[...state.log, value]});
+  }
+
     return (
         <div>
-            <input name="text" type="text"/>
+            <input name="text" type="text" onChange={logState}/>
+            {state.log.map((state)=> <div> {state}</div>)}
         </div>
     );
 }
